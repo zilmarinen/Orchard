8dd6d337617d7ff103b990f78b9b29f2093cb49e
Zack Brown <zack@zackbrown.co.uk>
2021-10-10 14:01:05 +0100

    Add tool colors

--- Orchard/Data Types/Tool.swift
+++ Orchard/Data Types/Tool.swift
@@ -45,19 +45,19 @@ enum Tool: String, Equatable, Identifiable {
         
         switch self {
             
-        case .bridges: return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .bushes: return Color(red: 0.0, green: 0.63, blue: 0.61)
-        case .buildings: return Color(red: 0.0, green: 0.63, blue: 0.61)
-        case .footpaths: return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .map : return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .portals: return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .rocks: return Color(red: 0.70, green: 0.64, blue: 0.64)
-        case .seams: return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .stairs: return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .surface: return Color(red: 0.23, green: 0.69, blue: 1.0)
-        case .trees: return Color(red: 0.43, green: 0.79, blue: 0.38)
-        case .walls: return Color(red: 1.0, green: 0.36, blue: 0.34)
-        case .water: return Color(red: 1.0, green: 0.36, blue: 0.34)
+        case .bridges: return Color(red: 0.670, green: 0.576, blue: 0.788)
+        case .bushes: return Color(red: 1, green: 0.470, blue: 0.470)
+        case .buildings: return Color(red: 0.474, green: 0.705, blue: 0.717)
+        case .footpaths: return Color(red: 0.458, green: 0.505, blue: 0.517)
+        case .map : return Color(red: 0.419, green: 0.478, blue: 0.631)
+        case .portals: return Color(red: 0.835, green: 0.494, blue: 0.494)
+        case .rocks: return Color(red: 0.588, green: 0.549, blue: 0.513)
+        case .seams: return Color(red: 0.8, green: 0.607, blue: 0.427)
+        case .stairs: return Color(red: 0.870, green: 0.537, blue: 0.443)
+        case .surface: return Color(red: 0.572, green: 0.588, blue: 0.490)
+        case .trees: return Color(red: 0.403, green: 0.541, blue: 0.454)
+        case .walls: return Color(red: 0.596, green: 0.427, blue: 0.556)
+        case .water: return Color(red: 0.192, green: 0.419, blue: 0.513)
         }
     }
     
